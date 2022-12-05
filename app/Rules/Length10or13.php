1eb5dada49765e25af0d62cdeff195a0efd8d8bf
Matt Greco <github@x.deedni.com>
2022-12-04 17:46:24 -0800

    minor tweaks

--- app/Rules/Length10or13.php
+++ app/Rules/Length10or13.php
@@ -8,7 +8,7 @@ class Length10or13 implements InvokableRule
 {
     public function __invoke($attribute, $value, $fail)
     {
-        if(strlen($value) != 10 && strlen($value) != 13) {
+        if (strlen($value) != 10 && strlen($value) != 13) {
             $fail(sprintf('Invalid :attribute: %s, length must be 10 or 13.', $value));
         }
     }
